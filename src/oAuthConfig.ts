b73c02eba19861bd3096e94e0f856efaf60ba47a
Matthew Conrad <conrad.matthewj@gmail.com>
2021-03-03 18:51:37 -0500

    Removing apiTokenUrl from the OAuth TS config.

--- src/oAuthConfig.ts
+++ src/oAuthConfig.ts
@@ -1,3 +1,2 @@
 export const authorizationUrl: string = "https://accounts.spotify.com/authorize";
-export const apiTokenUrl: string = "https://accounts.spotify.com/api/token";
 export const redirectUrl: string = "http://localhost:3000/callback";
