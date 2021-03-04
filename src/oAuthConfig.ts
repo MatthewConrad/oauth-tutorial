03e2e55c98af59834c957e9ebc83beabe94f4ad2
Matthew Conrad <conrad.matthewj@gmail.com>
2021-03-03 21:08:54 -0500

    Updated callback route to make request for access tokens.

--- src/oAuthConfig.ts
+++ src/oAuthConfig.ts
@@ -1,2 +1,3 @@
 export const authorizationUrl: string = "https://accounts.spotify.com/authorize";
+export const apiTokenUrl: string = "https://accounts.spotify.com/api/token";
 export const redirectUrl: string = "http://localhost:3000/callback";
--- src/oAuthRoutes.ts
+++ src/oAuthRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { authorizationUrl, redirectUrl } from "./oAuthConfig";
+import { apiTokenUrl, authorizationUrl, redirectUrl } from "./oAuthConfig";
+import fetch from "node-fetch";
 const oAuthRouter: Router = Router();
 
 oAuthRouter.get("/login", (_req, res) => {
@@ -12,13 +13,41 @@ oAuthRouter.get("/login", (_req, res) => {
     );
 });
 
-oAuthRouter.get("/callback", (req, res) => {
+oAuthRouter.get("/callback", async (req, res) => {
     if (!req.query.code) {
         res.status(400).send("Client did not send authorization code.");
     }
 
     const code: string = req.query.code as string;
-    res.status(200).send("Got an auth code!");
+    const authorization: string = Buffer.from(
+        `${process.env.OAUTH_CLIENT_ID}:${process.env.OAUTH_CLIENT_SECRET}`
+    ).toString("base64");
+    const authBody: URLSearchParams = new URLSearchParams({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: redirectUrl,
+    });
+
+    await fetch(apiTokenUrl, {
+        method: "POST",
+        headers: {
+            Authorization: `Basic ${authorization}`,
+            "content-type": "application/x-www-form-urlencoded",
+        },
+        body: authBody,
+    })
+        .then((response) => {
+            if (!response.ok) throw new Error("Failed to retrieve auth token.");
+            return response.json();
+        })
+        .then((response) => {
+            if (!response.access_token) throw new Error("Authorization was successful but no token was given.");
+            // tokens will be handled here
+            res.redirect("/"); // we could redirect elsewhere, but this is enough to tell us our token request was successful
+        })
+        .catch((error) => {
+            res.status(500).send(error.toString());
+        });
 });
 
 export default oAuthRouter;
